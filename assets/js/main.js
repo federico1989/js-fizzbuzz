e0a90f98a7e500f22e31ac17087ffbbff99e9ca6
Federico <federico.scarpati1989@gmail.com>
2020-11-19 15:50:27 +0100

    Aggiunta di tutti i valori
    Valori Fizz, Buzz e FizzBuzz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
-// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+for (var i = 1; i < 1000; i++) {
+  // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+  }else if (i % 3 === 0){
+    console.log("Fizz");
+  }else if (i % 5 === 0){
+    console.log("Buzz");
+  }else{
+    console.log(i);
+  }
+}
+
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
